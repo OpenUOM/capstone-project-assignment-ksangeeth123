df81048fb8fe1f0858b992b52f36518daad47cf0
Kavindu <ksangeeth179@gmail.com>
2023-05-06 03:57:19 -0700

    database function changes2

--- frontend/src/app/components/edit-student/edit-student.component.ts
+++ frontend/src/app/components/edit-student/edit-student.component.ts
@@ -1,43 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationExtras } from '@angular/router';
-import {AppServiceService} from '../../app-service.service';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-@Component({
-  selector: 'app-edit-student',
-  templateUrl: './edit-student.component.html',
-  styleUrls: ['./edit-student.component.css']
-})
-export class EditStudentComponent implements OnInit {
+import { EditStudentComponent } from './edit-student.component';
 
-  studentData: any;
+describe('EditStudentComponent', () => {
+  let component: EditStudentComponent;
+  let fixture: ComponentFixture<EditStudentComponent>;
 
-
-  constructor(private service : AppServiceService, private router: Router) { }
-
-  navigation = this.router.getCurrentNavigation();
-
-  ngOnInit(): void {
-    this.getStudentData();
-  }
-
-  getStudentData(){
-    let student = {
-      id : this.navigation.extras.state.id
-    }
-    this.service.getOneStudentData(student).subscribe((response)=>{
-      this.studentData = response[0];
-    },(error)=>{
-      console.log('ERROR - ', error)
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EditStudentComponent ]
     })
-  }
+    .compileComponents();
+  }));
 
-  editStudent(values){
-    values.id = this.navigation.extras.state.id;
-    this.service.editStudent(values).subscribe((response)=>{
-      this.studentData = response[0];
-    },(error)=>{
-      console.log('ERROR - ', error)
-    })
-  }
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditStudentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-}
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
